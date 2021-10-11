0c353cd210cc48b486ada3a7883b6c30ec0192c7
Alejandro Mery <amery@jpi.io>
2021-10-11 17:18:29 +0100

    nuxt: set png favicons
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+// vim: set ts=2 sw=2 et:
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -15,7 +16,8 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
     ]
   },
 
